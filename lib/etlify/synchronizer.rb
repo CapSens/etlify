91246cff69031817ccc00e273355d08fe583c246
Ismael Boukhars <ismael@capsens.eu>
2025-08-25 21:20:41 +0200

    [handle-multi-crm] feat: handle many crm on one app

--- lib/etlify/synchronizer.rb
+++ lib/etlify/synchronizer.rb
@@ -1,36 +1,43 @@
 module Etlify
   class Synchronizer
-    # main entry point
-    # @param record [ActiveRecord::Base]
-    def self.call(record)
-      new(record).call
+    attr_accessor(
+      :adapter,
+      :conf,
+      :crm_name,
+      :resource
+    )
+    # main entry point (CRM-aware)
+    # @param resource [ActiveRecord::Base]
+    # @param crm [Symbol,String]
+    def self.call(resource, crm_name:)
+      new(resource, crm_name: crm_name).call
     end
 
-    def initialize(record)
-      @record = record
+    def initialize(resource, crm_name:)
+      @resource = resource
+      @crm_name = crm_name.to_sym
+      @conf    = resource.class.etlify_crms.fetch(@crm_name)
+      @adapter = @conf[:adapter].new
     end
 
     def call
-      @record.with_lock do
+      resource.with_lock do
         if sync_line.stale?(digest)
-          crm_id = Etlify.config.crm_adapter.upsert!(
+          crm_id = adapter.upsert!(
             payload: payload,
             id_property: @record.etlify_id_property,
             object_type: @record.etlify_crm_object_type,
             crm_id: sync_line.crm_id.presence
           )
 
-          attributes = {
+          sync_line.update!(
+            crm_name: crm_name,
+            crm_id: crm_id.presence || sync_line.crm_id,
             last_digest: digest,
             last_synced_at: Time.current,
-            last_error: nil,
-          }
+            last_error: nil
+          )
 
-          unless sync_line.crm_id.present?
-            attributes[:crm_id] = crm_id.presence
-          end
-
-          sync_line.update!(attributes)
           :synced
         else
           sync_line.update!(last_synced_at: Time.current)
@@ -39,20 +46,27 @@ module Etlify
       end
     rescue => e
       sync_line.update!(last_error: e.message)
+
+      :error
     end
 
     private
 
+    # Compute once to keep idempotency inside the lock
     def digest
-      Etlify.config.digest_strategy.call(payload)
+      @digest ||= Etlify.config.digest_strategy.call(payload)
     end
 
     def payload
-      @__payload ||= @record.build_crm_payload
+      @payload ||= resource.build_crm_payload(crm_name: crm_name)
     end
 
+    # Select or build the per-CRM sync line.
+    # If you still have has_one, this keeps working but won't handle multi-CRM.
     def sync_line
-      @record.crm_synchronisation || @record.build_crm_synchronisation
+      resource.crm_synchronisations.find_or_initialize_by(
+        crm_name: crm_name
+      )
     end
   end
 end
